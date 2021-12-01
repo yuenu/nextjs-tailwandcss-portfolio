60153aefa176c2c0379542aa4edf77dcb6517bf9
Noah <yuenuni@hotmail.com>
2021-12-01 18:19:14 +0800

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,75 +1,66 @@
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { useState, useEffect, FunctionComponent } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-interface Props {
-  activeItem: string;
-  setActiveItem: Function;
-  name: string;
-  route: string;
+const NavItem: FunctionComponent<{
+   active: string
+   setActive: Function
+   name: string
+   route: string
+}> = ({ active, setActive, name, route }) => {
+   return active !== name ? (
+      <Link href={route}>
+         <a>
+            <span
+               className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
+               onClick={() => setActive(name)}>
+               {name}
+            </span>
+         </a>
+      </Link>
+   ) : null
 }
 
-const NavItem: React.FC<Props> = ({
-  activeItem,
-  setActiveItem,
-  name,
-  route,
-}) => {
-  return activeItem !== name ? (
-    <Link href={route}>
-      <a>
-        <span className="hover:text-green" onClick={() => setActiveItem(name)}>{name}</span>
-      </a>
-    </Link>
-  ) : null;
-};
-
 const Navbar = () => {
-  const [activeItem, setActiveItem] = useState<"About" | "Projects" | "Resume">(
-    "About"
-  );
+   const { pathname } = useRouter()
 
-  const { pathname } = useRouter();
+   const [active, setActive] = useState('')
 
-  useEffect(() => {
-    console.log('pathname:', pathname, pathname === "/")
-    switch(pathname) {
-      case '/projects':
-        setActiveItem("Projects")
-        break
-      case '/resume':
-        setActiveItem("Resume")
-        break
-      default:
-        setActiveItem("About");
-    }
-  }, [pathname]);
+   //later
+   useEffect(() => {
+      if (pathname === '/') setActive('About')
+      else if (pathname === '/projects') setActive('Projects')
+      else if (pathname === '/resume') setActive('Resume')
+   }, [])
 
-  return (
-    <div className="flex justify-between px-5 py-3 my-3">
-      <span className="text-xl font-bold border-b-4 text-green border-green md:text-2xl">{activeItem}</span>
-      <div className="flex space-x-5 text-lg text-red-400">
-        <NavItem
-          activeItem={activeItem}
-          setActiveItem={setActiveItem}
-          name="About"
-          route="/"
-        />
-        <NavItem
-          activeItem={activeItem}
-          setActiveItem={setActiveItem}
-          name="Projects"
-          route="/projects"
-        />
-        <NavItem
-          activeItem={activeItem}
-          setActiveItem={setActiveItem}
-          name="Resume"
-          route="/resume"
-        />
+   return (
+      <div className='flex items-center justify-between px-5 py-3 my-3'>
+         <span className='text-xl font-bold border-b-4 md:text-2xl border-green'>
+            {active}
+         </span>
+
+         <div className='text-base font-normal md:text-xl'>
+            <NavItem
+               active={active}
+               setActive={setActive}
+               name='About'
+               route='/'
+            />
+            <NavItem
+               active={active}
+               setActive={setActive}
+               name='Resume'
+               route='/resume'
+            />
+            <NavItem
+               active={active}
+               setActive={setActive}
+               name='Projects'
+               route='/projects'
+            />
+         </div>
       </div>
-    </div>
-  );
-};
+   )
+}
 
-export default Navbar;
+export default Navbar
